efc7cf34ef03e06c65f69ac718515c0f86a4f0c1
tastymeatball <44088711+tastymeatball@users.noreply.github.com>
2022-03-27 22:35:41 +0200

    Added pycryptodome package (Crypto) compatibility

--- cmac.py
+++ cmac.py
@@ -1,5 +1,9 @@
-from Cryptodome.Hash import CMAC
-from Cryptodome.Cipher import AES
+try:
+    from Crypto.Hash import CMAC
+    from Crypto.Cipher import AES
+except:
+    from Cryptodome.Hash import CMAC
+    from Cryptodome.Cipher import AES
 
 
 def AesCmac(data, key):
