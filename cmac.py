e1cc345aa75cd59e7be63d5ba2573c2098f8c96a
tastymeatball <44088711+tastymeatball@users.noreply.github.com>
2022-03-29 16:01:41 +0200

    Update cmac.py
    Changed order

--- cmac.py
+++ cmac.py
@@ -1,9 +1,9 @@
 try:
-    from Crypto.Hash import CMAC
-    from Crypto.Cipher import AES
-except:
     from Cryptodome.Hash import CMAC
     from Cryptodome.Cipher import AES
+except:
+    from Crypto.Hash import CMAC
+    from Crypto.Cipher import AES
 
 
 def AesCmac(data, key):
